af379371003a816c7f5906cedcc72596d4ecbe58
Jack Dougherty <jack.dougherty@trincoll.edu>
2024-03-21 13:22:23 -0400

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,6 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ikHA1Cz7_gDYxVDWMoRDCznB1nYlrj-h40YjQi3a39U/edit#gid=0';
-// But no longer relevant since storymap data is now archived in GitHub repo CSV folder
+var googleDocURL = null;
+// storymap data is now archived in GitHub repo CSV folder, migrated away from old sheet:
+// https://docs.google.com/spreadsheets/d/1ikHA1Cz7_gDYxVDWMoRDCznB1nYlrj-h40YjQi3a39U/edit#gid=0
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
