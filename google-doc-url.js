10ce5673b4b90f82d07dff649a4764b4a42fcd64
Jack Dougherty <jack.dougherty@trincoll.edu>
2024-03-21 11:06:27 -0400

    modified text for archived by

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,4 +1,5 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ikHA1Cz7_gDYxVDWMoRDCznB1nYlrj-h40YjQi3a39U/edit#gid=0';
+// But no longer relevant since storymap data is now archived in GitHub repo CSV folder
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
